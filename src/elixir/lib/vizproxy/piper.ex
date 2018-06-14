35cb63148b503dcb562e5c16175c9ea80d4052be
zakora <zakora@users.noreply.github.com>
2018-06-14 21:26:31 +0200

    Elixir: remove frame state from Piper
    Previously, each new frame was stored in the Piper state and were never removed.
    Since Piper job is to just pass the frames along, its state is now nil.

--- src/elixir/lib/vizproxy/piper.ex
+++ src/elixir/lib/vizproxy/piper.ex
@@ -8,24 +8,19 @@ defmodule VizProxy.Piper do
     GenServer.start_link(__MODULE__, :ok, opts)
   end
 
-  def pending_frames() do
-    GenServer.call(Piper, :list_all)
-  end
-
   def parse_loop() do
     Logger.debug(fn -> "Starting parse_loop()" end)
     Enum.each IO.stream(:stdio, :line), &GenServer.call(Piper, {:new_frame, &1})
   end
 
   def init(:ok) do
-    # TODO use a queue instead of a list
-    {:ok, []}
+    # NOTE maybe use a :queue to serve as a buffer?
+    {:ok, nil}
   end
 
-  def handle_call({:new_frame, data}, _from, frames) do
+  def handle_call({:new_frame, data}, _from, state) do
     Logger.debug(fn -> "got new frame with: #{data}" end)
     Logger.debug(fn -> "pids: #{inspect Registry.lookup(VizProxy.Registry, "ws_connections")}" end)
-    Logger.debug(fn -> "state (frames): #{inspect frames}" end)
 
     data = String.trim data
 
@@ -33,11 +28,6 @@ defmodule VizProxy.Piper do
       for {pid, _} <- entries, do: send(pid, data)
     end)
 
-    frames = frames ++ [data]
-    {:reply, frames, frames}
-  end
-
-  def handle_call(:list_all, _from, frames) do
-    {:reply, frames, frames}
+    {:reply, state, state}
   end
 end
